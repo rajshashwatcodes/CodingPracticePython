775982404fe6bd940621e455b0ea5db4c11c6e82
Shashwat Raj <113779833+rajshashwatcodes@users.noreply.github.com>
2023-06-14 23:03:33 +0530

    Create Graph_cycle.py

---
+++ General/Graph_cycle.py
@@ -0,0 +1,19 @@
+def has_cycle(graph):
+    visited = set()
+
+    def dfs(node, parent):
+        visited.add(node)
+        for neighbor in graph[node]:
+            if neighbor not in visited:
+                if dfs(neighbor, node):
+                    return True
+            elif neighbor != parent:
+                return True
+        return False
+
+    for node in graph:
+        if node not in visited:
+            if dfs(node, None):
+                return True
+
+    return False
