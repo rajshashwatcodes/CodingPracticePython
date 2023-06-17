16115c053845ab485ebd11a27f9d866720e53891
Shashwat Raj <113779833+rajshashwatcodes@users.noreply.github.com>
2023-06-17 23:51:52 +0530

    Create delete_list.py

---
+++ General/delete_list.py
@@ -0,0 +1,13 @@
+# Set a list
+my_list = [1, 2, 3, 4, 5]
+# Print list
+print(my_list)
+# Delete element from list
+del my_list[2]
+# Print list
+print(my_list)
+# Delete element from list
+del my_list[3]
+# Print list
+print(my_list)
+
