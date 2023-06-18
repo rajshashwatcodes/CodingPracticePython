4e36ee7b1f31958d51539175f0917f694687c632
Shashwat Raj <113779833+rajshashwatcodes@users.noreply.github.com>
2023-06-18 23:56:01 +0530

    Create append_list.py

---
+++ General/append_list.py
@@ -0,0 +1,19 @@
+# Set a list
+my_list = [1, 2, 3, 4, 5]
+# Print list
+print(my_list)
+# Delete element from list
+del my_list[2]
+# Print list
+print(my_list)
+
+# add element to list
+my_list.append(6)
+# Print list
+print(my_list)
+
+# add element to list
+my_list.append(7)
+# Print list
+print(my_list)
+
