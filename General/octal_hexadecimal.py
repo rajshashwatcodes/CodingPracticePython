1199b6f1ecca137f692e3138e5be5c616a1fd7ef
Shashwat Raj <113779833+rajshashwatcodes@users.noreply.github.com>
2023-07-07 23:47:28 +0530

    Create octal_hexadecimal.py

---
+++ General/octal_hexadecimal.py
@@ -0,0 +1,10 @@
+# Prompt the user to enter a number
+decimal = int(input("Enter a decimal number: "))
+
+# Convert the number to octal and hexadecimal
+octal = oct(decimal)
+hexadecimal = hex(decimal)
+
+# Print the octal and hexadecimal equivalents
+print("Octal:", octal)
+print("Hexadecimal:", hexadecimal)
