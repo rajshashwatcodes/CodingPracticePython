0c235479fad38f66dd6bf3ffff9410bd69682e28
Shashwat Raj <113779833+rajshashwatcodes@users.noreply.github.com>
2023-07-20 23:44:45 +0530

    Create doubly_linked_list.py

---
+++ General/doubly_linked_list.py
@@ -0,0 +1,33 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.prev = None
+        self.next = None
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+        else:
+            current = self.head
+            while current.next:
+                current = current.next
+            current.next = new_node
+            new_node.prev = current
+
+    def prepend(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+        else:
+            new_node.next = self.head
+            self.head.prev = new_node
+            self.head = new_node
+
+    def insert_after(self, prev_node, data):
+        if prev_node is None:
+            print("Previous node is not present.")
