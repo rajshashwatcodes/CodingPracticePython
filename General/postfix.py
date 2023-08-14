69927128c56ee7006408d9821257c6b6b3b040f9
Shashwat Raj <113779833+rajshashwatcodes@users.noreply.github.com>
2023-08-14 23:27:17 +0530

    Create postfix.py

---
+++ General/postfix.py
@@ -0,0 +1,24 @@
+def evaluate_postfix(expression):
+    stack = []
+
+    for char in expression:
+        if char.isdigit():
+            stack.append(int(char))
+        else:
+            operand2 = stack.pop()
+            operand1 = stack.pop()
+            if char == '+':
+                stack.append(operand1 + operand2)
+            elif char == '-':
+                stack.append(operand1 - operand2)
+            elif char == '*':
+                stack.append(operand1 * operand2)
+            elif char == '/':
+                stack.append(operand1 / operand2)
+
+    return stack[0]
+
+postfix_expression = "342*+"
+result = evaluate_postfix(postfix_expression)
+print(result)  
+
